ff3984f3cbb5ef11268e7e1a3ee149d9d9f28643
i-am-vaibhav <vg566556@gmail.com>
2025-05-07 22:51:10 +0530

    Implement module federation in Vite configuration for frontend applications

--- frontend/rhythmix-player-web/vite.config.ts
+++ frontend/rhythmix-player-web/vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "player_web",
+      filename: "remoteEntry.js",
+      exposes: {
+        
+      },
+      shared: {
+        "react": {
+          singleton: true,
+          requiredVersion: "^19.1.0",
+        },
+        "react-icons": {
+          singleton: true,
+          requiredVersion: "^5.5.0",
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: "^19.1.0",
+        },
+        "jotai": {
+          singleton: true,
+          requiredVersion: "^2.12.4",
+        },
+      }, 
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  preview: {
+    host: "localhost",
+    port: 3002,
+    strictPort: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+});
--- frontend/rhythmix-web/vite.config.ts
+++ frontend/rhythmix-web/vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      federation({
+        name: "container",
+        remotes: {
+          home_web: "http://localhost:3000/assets/remoteEntry.js",
+          library_web: "http://localhost:3001/assets/remoteEntry.js",
+          player_web: "http://localhost:3002/assets/remoteEntry.js",
+        },
+        shared: {
+          "react": {
+            singleton: true,
+            requiredVersion: "^19.1.0",
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: "^19.1.0",
+          },
+          "react-router-dom": {
+            singleton: true,
+            requiredVersion: "^6.0.0",
+          },
+          "jotai": {
+            singleton: true,
+            requiredVersion: "^2.12.4",
+          },
+          "react-icons": {
+            singleton: true,
+            requiredVersion: "^5.5.0",
+          }
+        },      
+      }),
+    ],
+    build: {
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
 })
